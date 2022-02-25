fdbaaa97e6ca843ce639642f2fba59cb38ba3dbb
camilord-end <jcorozcoa@correo.udistrital.edu.co>
2022-02-25 09:24:54 -0500

    refreshing concepts

---
+++ Course/3.py
@@ -0,0 +1,5 @@
+hours= input('Enter Hours:')
+rate= input('Enter Rate:')
+payment= float(hours)*float(rate)
+
+print('Pay:',payment)
---
+++ Course/4.py
@@ -0,0 +1,5 @@
+x=0
+y=10
+if x==0:
+    if y==10:
+        print("Yes")
